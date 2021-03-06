66b73aefb7970bae3fbcb37a2361c1d34702519c
toru <toru@shanghai.mshome.net>
2021-03-07 01:39:08 +0900

    Add error function

--- ndp-ns-show.c
+++ ndp-ns-show.c
@@ -18,27 +18,29 @@
 
 #include <err.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <ifaddrs.h>
 #include <poll.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <stdarg.h>
 
 struct if_info {
   int ii_fd;                       /* BPF file descriptor */
   char ii_name[IFNAMSIZ];          /* if name, e.g. "en0" */
   u_char ii_eaddr[ETHER_ADDR_LEN]; /* Ethernet address of this iface */
-  u_char *buf; // bpf read buffer
-  size_t buf_max; // Allocated buffer size
+  u_char *buf;                     // bpf read buffer
+  size_t buf_max;                  // Allocated buffer size
 } ii;
 
 void lookup_addrs(char *);
 int open_bpf(char *);
-void debug(const char *,...);
+void error(const char *, ...);
+void errorx(const char *, ...);
+void debug(const char *, ...);
 
 int dflag = 1;
 
@@ -62,10 +64,10 @@ static struct bpf_insn insns[] = {
 
     /* Make sure this is an Neighbor Discovery packet. */
     BPF_STMT(BPF_LD + BPF_B + BPF_ABS, 54),
-	BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ND_ROUTER_SOLICIT, 4, 0),
-	BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ND_ROUTER_ADVERT, 3, 0),
-	BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ND_NEIGHBOR_SOLICIT, 2, 0),
-	BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ND_NEIGHBOR_ADVERT, 1, 0),
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ND_ROUTER_SOLICIT, 4, 0),
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ND_ROUTER_ADVERT, 3, 0),
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ND_NEIGHBOR_SOLICIT, 2, 0),
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ND_NEIGHBOR_ADVERT, 1, 0),
     BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ND_REDIRECT, 0, 1),
 
     /* If we passed all the tests, ask for the whole packet. */
@@ -86,50 +88,52 @@ int open_bpf(char *if_name) {
   u_int dlt;
 
   if ((fd = open("/dev/bpf", O_RDWR)) == -1)
-    err(1, "open /dev/bpf:");
+    error("open /dev/bpf:");
 
   debug("Open BPF file descriptor: %d", fd);
 
   /* Set immediate mode so packets are processed as they arrive. */
   immediate = 1;
   if (ioctl(fd, BIOCIMMEDIATE, &immediate) == -1)
-    err(1, "ioctl(BIOCIMMEDIATE)");
+    error("ioctl(BIOCIMMEDIATE)");
 
   /* Get the BPF buffer length. */
   if (ioctl(fd, BIOCGBLEN, &sz) == -1)
-    err(1, "ioctl(BIOCGBLEN)");
+    error("ioctl(BIOCGBLEN)");
 
   /* Allocate buffer for BPF read */
   ii.buf_max = sz;
   ii.buf = malloc(ii.buf_max);
-  if(!ii.buf)
-    errx(1, "malloc for BFP buffer %zd byte", ii.buf_max);
+  if (!ii.buf)
+    errorx("malloc for BFP buffer %zd byte", ii.buf_max);
 
   /* Associate a hardware interface to BPF descriptor. */
   strncpy(ifr.ifr_name, if_name, sizeof(ifr.ifr_name));
   if (ioctl(fd, BIOCSETIF, (caddr_t)&ifr) == -1)
-    err(1, "ioctl(BIOCSETIF)");
+    error("ioctl(BIOCSETIF)");
 
   /*
-   * Check that the data link layer is an Ethernet; this code won't work with anything else.
+   * Check that the data link layer is an Ethernet; this code won't work with
+   * anything else.
    */
-  if (ioctl(fd, BIOCGDLT, (caddr_t) &dlt) == -1)
+  if (ioctl(fd, BIOCGDLT, (caddr_t)&dlt) == -1)
     err(1, "ioctl(BIOCGDLT)");
   if (dlt != DLT_EN10MB)
-    errx(1, "%s is not an Ethernet interface.", if_name);
+    error("%s is not an Ethernet interface.", if_name);
 
   /* Set filter program. */
   if (ioctl(fd, BIOCSETF, (caddr_t)&filter) == -1)
-    err(1, "ioctl(BIOCSETF)");
+    error("ioctl(BIOCSETF)");
 
   /* Set direction filter to ignore outgoing packets. */
   flag = BPF_DIRECTION_OUT;
-  if (ioctl(fd, BIOCSDIRFILT , &flag) == -1)
-    err(1, "ioctl(BIOCSDIRFILT)");
+  if (ioctl(fd, BIOCSDIRFILT, &flag) == -1)
+    error("ioctl(BIOCSDIRFILT)");
 
-  /* Lock the BPF descriptor to prevent the security issues after dropping privileges. */
+  /* Lock the BPF descriptor to prevent the security issues after dropping
+   * privileges. */
   if (ioctl(fd, BIOCLOCK) == -1)
-    err(1, "ioctl(BIOCLOCK)");
+    error("ioctl(BIOCLOCK)");
 
   return fd;
 }
@@ -149,7 +153,7 @@ void lookup_addrs(char *if_name) {
   char ntop_buf[INET6_ADDRSTRLEN];
 
   if (getifaddrs(&ifap) != 0)
-    err(1, "getifaddrs");
+    error("getifaddrs");
 
   for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
 
@@ -164,9 +168,8 @@ void lookup_addrs(char *if_name) {
       sdl = (struct sockaddr_dl *)ifa->ifa_addr;
       if (sdl->sdl_type == IFT_ETHER && sdl->sdl_alen == 6) {
         memcpy((caddr_t)eaddr, (caddr_t)LLADDR(sdl), ETHER_ADDR_LEN);
-        debug("%s [Ethernet]: %02x:%02x:%02x:%02x:%02x:%02x",
-              ifa->ifa_name, eaddr[0], eaddr[1], eaddr[2], eaddr[3],
-              eaddr[4], eaddr[5]);
+        debug("%s [Ethernet]: %02x:%02x:%02x:%02x:%02x:%02x", ifa->ifa_name,
+              eaddr[0], eaddr[1], eaddr[2], eaddr[3], eaddr[4], eaddr[5]);
         found = 1;
       }
       break;
@@ -179,7 +182,7 @@ void lookup_addrs(char *if_name) {
     case AF_INET6:
       sin6 = (struct sockaddr_in6 *)ifa->ifa_addr;
       if (!inet_ntop(AF_INET6, &sin6->sin6_addr, ntop_buf, sizeof(ntop_buf)))
-        err(1, "inet_ntop");
+        error("inet_ntop");
       debug("%s [IPv6]: %s", ifa->ifa_name, ntop_buf);
       break;
     }
@@ -187,20 +190,45 @@ void lookup_addrs(char *if_name) {
   freeifaddrs(ifap);
 
   if (!found)
-    errx(1, "Interface not found %s\n", if_name);
+    errorx("Interface not found %s\n", if_name);
 }
 
-void
-debug(const char *fmt,...)
-{
-	va_list ap;
+void ndp_show_loop(void) {}
 
-	if (dflag) {
-		va_start(ap, fmt);
-		(void) fprintf(stderr, "ndp-show: ");
-		(void) vfprintf(stderr, fmt, ap);
-		va_end(ap);
-		(void) fprintf(stderr, "\n");
-	}
+__dead void error(const char *fmt, ...) {
+  va_list ap;
+
+  if (dflag) {
+    (void)fprintf(stderr, "rarpd: error: ");
+    va_start(ap, fmt);
+    (void)vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    (void)fprintf(stderr, ":%s\n", strerror(errno));
+  }
+  exit(1);
 }
 
+__dead void errorx(const char *fmt, ...) {
+  va_list ap;
+
+  if (dflag) {
+    (void)fprintf(stderr, "rarpd: error: ");
+    va_start(ap, fmt);
+    (void)vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    (void)fprintf(stderr, "\n");
+  }
+  exit(1);
+}
+
+void debug(const char *fmt, ...) {
+  va_list ap;
+
+  if (dflag) {
+    va_start(ap, fmt);
+    (void)fprintf(stderr, "ndp-show: ");
+    (void)vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    (void)fprintf(stderr, "\n");
+  }
+}
